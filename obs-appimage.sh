439686f6a303236bdad676bf58b38e9a48a93f9f
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-11-07 12:48:58 -0400

    use lib4bin to download sharun

--- obs-appimage.sh
+++ obs-appimage.sh
@@ -13,8 +13,6 @@ export APPIMAGE_EXTRACT_AND_RUN=1
 APPIMAGETOOL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$ARCH.AppImage"
 UPINFO="gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|OBS-Studio-AppImage|continuous|*$ARCH.AppImage.zsync"
 LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
-SHARUN=$(wget -q https://api.github.com/repos/VHSgunzo/sharun/releases -O - \
-	| sed 's/[()",{} ]/\n/g' | grep -oi "https.*-$ARCH$" | head -1)
 
 # Prepare AppDir
 mkdir -p ./"$PACKAGE"/AppDir/usr/share/applications
@@ -36,9 +34,8 @@ chmod +x ./AppRun
 
 # ADD LIBRARIES
 wget "$LIB4BN" -O ./lib4bin
-wget "$SHARUN" -O ./sharun
-chmod +x ./lib4bin ./sharun
-HARD_LINKS=1 ./lib4bin /usr/bin/obs*
+chmod +x ./lib4bin
+./lib4bin -p -w -v /usr/bin/obs*
 rm -f ./lib4bin
 
 ln -s ./shared/lib ./lib # NEEDED BY OBS
