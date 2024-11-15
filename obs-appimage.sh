ce20a63c053a04aa0991d0f36171e99143783a4d
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-11-15 17:03:26 -0400

    make dwarfs AppImage with new uruntime

--- obs-appimage.sh
+++ obs-appimage.sh
@@ -10,9 +10,10 @@ ICON=com.obsproject.Studio.png
 export ARCH="$(uname -m)"
 export APPIMAGE_EXTRACT_AND_RUN=1
 
-APPIMAGETOOL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$ARCH.AppImage"
 UPINFO="gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|OBS-Studio-AppImage|continuous|*$ARCH.AppImage.zsync"
 LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
+URUNTIME="$(wget -q https://api.github.com/repos/VHSgunzo/uruntime/releases -O - \
+	| sed 's/[()",{} ]/\n/g' | grep -oi "https.*appimage.*dwarfs.*$ARCH$" | head -1)"
 
 # Prepare AppDir
 mkdir -p ./"$PACKAGE"/AppDir/usr/share/applications ./"$PACKAGE"/AppDir/shared/lib
@@ -58,14 +59,23 @@ ldd ./shared/lib/qt6/plugins/*/* 2>/dev/null \
 find ./shared/lib -type f -exec strip -s -R .comment --strip-unneeded {} ';'
 ./sharun -g
 
-# MAKE APPIAMGE WITH FUSE3 COMPATIBLE APPIMAGETOOL
-cd ..
-wget -q "$APPIMAGETOOL" -O ./appimagetool
-chmod +x ./appimagetool
+#Add udpate info to runtime
+echo "Adding update information \"$UPINFO\" to runtime..."
+printf "$UPINFO" > data.upd_info
+llvm-objcopy --update-section=.upd_info=data.upd_info \
+	--set-section-flags=.upd_info=noload,readonly ./uruntime
+printf 'AI\x02' | dd of=./uruntime bs=1 count=3 seek=8 conv=notrunc
 
-./appimagetool --comp zstd \
-	--mksquashfs-opt -Xcompression-level --mksquashfs-opt 22 \
-	-n -u "$UPINFO" "$PWD"/AppDir "$PWD"/"$PACKAGE"-"$VERSION"-"$ARCH".AppImage
+echo "Generating AppImage..."
+./uruntime --appimage-mkdwarfs -f \
+	--set-owner 0 --set-group 0 \
+	--no-history --no-create-timestamp \
+	--compression zstd:level=22 -S24 -B16 \
+	--header uruntime \
+	-i ./AppDir -o "$PACKAGE"-"$VERSION"-"$ARCH".AppImage
+
+echo "Generating zsync file..."
+zsyncmake *.AppImage -u *.AppImage
 
 mv ./*.AppImage* ../
 cd ..
