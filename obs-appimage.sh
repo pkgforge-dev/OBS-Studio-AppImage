0b7988686c149cefacfb8c634a4f96dcbb6c2a9a
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2024-11-16 21:01:28 -0400

    Update obs-appimage.sh

--- obs-appimage.sh
+++ obs-appimage.sh
@@ -3,14 +3,14 @@
 set -eu
 
 PACKAGE=obs-studio
-TARGET_BIN=obs
 DESKTOP=com.obsproject.Studio.desktop
 ICON=com.obsproject.Studio.png
 
 export ARCH="$(uname -m)"
 export APPIMAGE_EXTRACT_AND_RUN=1
+export VERSION="$(pacman -Q $PACKAGE | awk 'NR==1 {print $2; exit}')"
 
-UPINFO="gh-releases-zsync|$GITHUB_REPOSITORY_OWNER|OBS-Studio-AppImage|continuous|*$ARCH.AppImage.zsync"
+UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|continuous|*$ARCH.AppImage.zsync"
 LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
 URUNTIME="$(wget -q https://api.github.com/repos/VHSgunzo/uruntime/releases -O - \
 	| sed 's/[()",{} ]/\n/g' | grep -oi "https.*appimage.*dwarfs.*$ARCH$" | head -1)"
@@ -18,33 +18,23 @@ URUNTIME="$(wget -q https://api.github.com/repos/VHSgunzo/uruntime/releases -O -
 # Prepare AppDir
 mkdir -p ./"$PACKAGE"/AppDir/usr/share/applications ./"$PACKAGE"/AppDir/shared/lib
 cd ./"$PACKAGE"/AppDir
-cp -r /usr/share/obs ./usr/share
-cp -r /usr/share/locale ./usr/share
-cp -r /usr/share/glvnd ./usr/share
-cp /usr/share/applications/$DESKTOP ./usr/share/applications
-cp /usr/share/applications/$DESKTOP ./
-cp /usr/share/icons/hicolor/256x256/apps/"$ICON" ./
-ln -s ./usr/share ./share
-ln -s ./shared/lib ./lib
 
-echo '#!/bin/sh
-CURRENTDIR="$(dirname "$(readlink -f "$0")")"
-export XDG_DATA_DIRS="$CURRENTDIR/usr/share:$XDG_DATA_DIRS"
-export __EGL_VENDOR_LIBRARY_DIRS="$CURRENTDIR/share/glvnd/egl_vendor.d"
-export PATH="$CURRENTDIR/bin:$PATH"
-"$CURRENTDIR"/bin/TARGET "$@"' > ./AppRun
-sed -i "s|TARGET|$TARGET_BIN|" ./AppRun
-chmod +x ./AppRun
+cp -r /usr/share/obs                             ./usr/share
+cp -r /usr/share/locale                          ./usr/share
+cp -r /usr/share/glvnd                           ./usr/share
+cp /usr/share/applications/$DESKTOP              ./usr/share/applications
+cp /usr/share/applications/$DESKTOP              ./
+cp /usr/share/icons/hicolor/256x256/apps/"$ICON" ./
+
+ln -s ./usr/share ./
 
 # ADD LIBRARIES
 wget "$LIB4BN" -O ./lib4bin
 chmod +x ./lib4bin
-xvfb-run -d -- ./lib4bin -p -v -e -r -s /usr/bin/obs*
+xvfb-run -d -- ./lib4bin -p -v -e -r /usr/bin/obs*
 rm -f ./lib4bin
 
-export VERSION=$(pacman -Q $PACKAGE | awk 'NR==1 {print $2; exit}')
-
-# DELOY QT
+# DELOY QT AND OBS PLUGINS
 mkdir -p ./shared/lib/qt6/plugins
 cp -r /usr/lib/qt6/plugins/iconengines       ./shared/lib/qt6/plugins
 cp -r /usr/lib/qt6/plugins/imageformats      ./shared/lib/qt6/plugins
@@ -54,9 +44,18 @@ cp -r /usr/lib/qt6/plugins/styles            ./shared/lib/qt6/plugins
 cp -r /usr/lib/qt6/plugins/xcbglintegrations ./shared/lib/qt6/plugins
 cp -r /usr/lib/qt6/plugins/wayland-*         ./shared/lib/qt6/plugins
 
-ldd ./shared/lib/qt6/plugins/*/* 2>/dev/null \
+cp -r /usr/lib/obs-plugins   ./shared/lib
+cp -r /usr/lib/obs-scripting ./shared/lib
+
+ldd ./shared/lib/qt6/plugins/*/* ./shared/lib/obs*/* 2>/dev/null \
   | awk -F"[> ]" '{print $4}' | xargs -I {} cp -nv {} ./shared/lib || true
-find ./shared/lib -type f -exec strip -s -R .comment --strip-unneeded {} ';'
+
+find ./shared -type f -exec strip -s -R .comment --strip-unneeded {} ';'
+
+# Prepare sharun
+echo '#!/bin/sh
+CURRENTDIR="$(dirname "$(readlink -f "$0")")"
+exec "$CURRENTDIR"/bin/obs "$@"' > ./AppRun
 ./sharun -g
 
 # MAKE APPIMAGE WITH URUNTIME
