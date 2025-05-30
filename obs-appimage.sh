086d553c77b11924ee004234ce9fc4cf5c310913
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-05-29 20:24:16 -0400

    Update obs-appimage.sh

--- obs-appimage.sh
+++ obs-appimage.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-set -eu
+set -eux
 
 PACKAGE=obs-studio
 DESKTOP=com.obsproject.Studio.desktop
 ICON=com.obsproject.Studio.png
 
-export ARCH="$(uname -m)"
-export APPIMAGE_EXTRACT_AND_RUN=1
-export VERSION="$(pacman -Q $PACKAGE | awk 'NR==1 {print $2; exit}')"
+ARCH="$(uname -m)"
+VERSION="$(pacman -Q $PACKAGE | awk 'NR==1 {print $2; exit}')"
 echo "$VERSION" > ~/version
 
 UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
-LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
+SHARUN="https://github.com/VHSgunzo/sharun/releases/latest/download/sharun-$ARCH-aio"
 URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
 
 # Prepare AppDir
@@ -21,15 +20,14 @@ cd ./AppDir
 
 cp -r /usr/share/obs                              ./share
 cp -r /usr/share/locale                           ./share
-cp -r /usr/share/glvnd                            ./share
 cp /usr/share/applications/"$DESKTOP"             ./
 cp /usr/share/icons/hicolor/256x256/apps/"$ICON"  ./
 cp /usr/share/icons/hicolor/256x256/apps/"$ICON"  ./.DirIcon
 ln -s ./ ./usr
 
 # ADD LIBRARIES
-wget "$LIB4BN" -O ./lib4bin
-chmod +x ./lib4bin
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./sharun-aio
+chmod +x ./sharun-aio
 xvfb-run -a -- ./lib4bin -p -v -e -k -s \
 	/usr/bin/obs* \
 	/usr/lib/libobs* \
@@ -52,7 +50,7 @@ xvfb-run -a -- ./lib4bin -p -v -e -k -s \
 	/usr/lib/alsa-lib/* \
 	/usr/lib/pipewire-0.3/* \
 	/usr/lib/spa-0.2/*/*
-
+rm -f ./sharun-aio
 cp -vn /usr/lib/obs-scripting/* ./shared/lib/obs-scripting
 
 # Prepare sharun
@@ -61,23 +59,21 @@ ln ./sharun ./AppRun
 
 # MAKE APPIMAGE WITH URUNTIME
 cd ..
-wget -q "$URUNTIME" -O ./uruntime
-chmod +x ./uruntime
+wget --retry-connrefused --tries=30 "$URUNTIME"      -O  ./uruntime
+wget --retry-connrefused --tries=30 "$URUNTIME_LITE" -O  ./uruntime-lite
+chmod +x ./uruntime*
 
-#Add udpate info to runtime
+# Add udpate info to runtime
 echo "Adding update information \"$UPINFO\" to runtime..."
-printf "$UPINFO" > data.upd_info
-llvm-objcopy --update-section=.upd_info=data.upd_info \
-	--set-section-flags=.upd_info=noload,readonly ./uruntime
-printf 'AI\x02' | dd of=./uruntime bs=1 count=3 seek=8 conv=notrunc
+./uruntime-lite --appimage-addupdinfo "$UPINFO"
 
 echo "Generating AppImage..."
 ./uruntime --appimage-mkdwarfs -f \
 	--set-owner 0 --set-group 0 \
 	--no-history --no-create-timestamp \
-	--compression zstd:level=22 -S26 -B32 \
-	--header uruntime \
-	-i ./AppDir -o OBS-Studio-"$VERSION"-anylinux-"$ARCH".AppImage
+	--compression zstd:level=22 -S26 -B8 \
+	--header uruntime-lite \
+	-i ./AppDir -o ./OBS-Studio-"$VERSION"-anylinux-"$ARCH".AppImage
 
 echo "Generating zsync file..."
 zsyncmake *.AppImage -u *.AppImage
