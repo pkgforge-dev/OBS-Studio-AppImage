033aeacdc7df58a04ce3d2e5979089eb2eaacdc2
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-05-30 01:41:36 +0000

    bring to proper standard (#11)

--- obs-appimage.sh
+++ obs-appimage.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-set -eu
+set -eux
 
 PACKAGE=obs-studio
 DESKTOP=com.obsproject.Studio.desktop
 ICON=com.obsproject.Studio.png
 
-export ARCH="$(uname -m)"
-export APPIMAGE_EXTRACT_AND_RUN=1
-export VERSION="$(pacman -Q $PACKAGE | awk 'NR==1 {print $2; exit}')"
+ARCH="$(uname -m)"
+VERSION="$(pacman -Q $PACKAGE | awk 'NR==1 {print $2; exit}')"
 echo "$VERSION" > ~/version
 
 UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|latest|*$ARCH.AppImage.zsync"
-LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
+SHARUN="https://github.com/VHSgunzo/sharun/releases/latest/download/sharun-$ARCH-aio"
 URUNTIME="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-$ARCH"
+URUNTIME_LITE="https://github.com/VHSgunzo/uruntime/releases/latest/download/uruntime-appimage-dwarfs-lite-$ARCH"
 
 # Prepare AppDir
 mkdir -p ./AppDir/share
@@ -21,16 +21,16 @@ cd ./AppDir
 
 cp -r /usr/share/obs                              ./share
 cp -r /usr/share/locale                           ./share
-cp -r /usr/share/glvnd                            ./share
+find ./share/locale -type f ! -name '*glib*' ! -name '*v4l*' ! -name '*obs*' -delete
 cp /usr/share/applications/"$DESKTOP"             ./
 cp /usr/share/icons/hicolor/256x256/apps/"$ICON"  ./
 cp /usr/share/icons/hicolor/256x256/apps/"$ICON"  ./.DirIcon
 ln -s ./ ./usr
 
 # ADD LIBRARIES
-wget "$LIB4BN" -O ./lib4bin
-chmod +x ./lib4bin
-xvfb-run -a -- ./lib4bin -p -v -e -k -s \
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./sharun-aio
+chmod +x ./sharun-aio
+xvfb-run -a -- ./sharun-aio l -p -v -e -k -s \
 	/usr/bin/obs* \
 	/usr/lib/libobs* \
 	/usr/lib/obs-plugins/* \
@@ -52,7 +52,7 @@ xvfb-run -a -- ./lib4bin -p -v -e -k -s \
 	/usr/lib/alsa-lib/* \
 	/usr/lib/pipewire-0.3/* \
 	/usr/lib/spa-0.2/*/*
-
+rm -f ./sharun-aio
 cp -vn /usr/lib/obs-scripting/* ./shared/lib/obs-scripting
 
 # Prepare sharun
@@ -61,25 +61,34 @@ ln ./sharun ./AppRun
 
 # MAKE APPIMAGE WITH URUNTIME
 cd ..
-wget -q "$URUNTIME" -O ./uruntime
-chmod +x ./uruntime
+wget --retry-connrefused --tries=30 "$URUNTIME"      -O  ./uruntime
+wget --retry-connrefused --tries=30 "$URUNTIME_LITE" -O  ./uruntime-lite
+chmod +x ./uruntime*
 
-#Add udpate info to runtime
+# Add udpate info to runtime
 echo "Adding update information \"$UPINFO\" to runtime..."
-printf "$UPINFO" > data.upd_info
-llvm-objcopy --update-section=.upd_info=data.upd_info \
-	--set-section-flags=.upd_info=noload,readonly ./uruntime
-printf 'AI\x02' | dd of=./uruntime bs=1 count=3 seek=8 conv=notrunc
+./uruntime-lite --appimage-addupdinfo "$UPINFO"
 
 echo "Generating AppImage..."
 ./uruntime --appimage-mkdwarfs -f \
 	--set-owner 0 --set-group 0 \
 	--no-history --no-create-timestamp \
-	--compression zstd:level=22 -S26 -B32 \
-	--header uruntime \
-	-i ./AppDir -o OBS-Studio-"$VERSION"-anylinux-"$ARCH".AppImage
+	--compression zstd:level=22 -S26 -B8 \
+	--header uruntime-lite \
+	-i ./AppDir -o ./OBS-Studio-"$VERSION"-anylinux-"$ARCH".AppImage
 
-echo "Generating zsync file..."
-zsyncmake *.AppImage -u *.AppImage
+# make appbundle
+UPINFO="gh-releases-zsync|$(echo "$GITHUB_REPOSITORY" | tr '/' '|')|latest|*$ARCH*.AppBundle.zsync"
+wget -qO ./pelf "https://github.com/xplshn/pelf/releases/latest/download/pelf_$ARCH"
+chmod +x ./pelf
+echo "Generating [dwfs]AppBundle...(Go runtime)"
+./pelf --add-appdir ./AppDir \
+	--compression "-C zstd:level=22 -S26 -B8" \
+	--appbundle-id="OBS-Studio-$VERSION" \
+	--appimage-compat --disable-use-random-workdir \
+	--add-updinfo "$UPINFO" \
+	--output-to "OBS-Studio-$VERSION-anylinux-$ARCH.dwfs.AppBundle"
 
+zsyncmake ./*.AppImage -u ./*.AppImage
+zsyncmake ./*.AppBundle -u ./*.AppBundle
 echo "All Done!"
