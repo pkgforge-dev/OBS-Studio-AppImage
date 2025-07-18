1969711d5eca93e67e6265dc4501d104a8af4afc
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-07-18 00:34:03 -0400

    add `libva-intel-driver` and use `intel-media-mini`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -2,8 +2,6 @@
 
 set -eux
 
-sed -i 's/DownloadUser/#DownloadUser/g' /etc/pacman.conf
-
 if [ "$(uname -m)" = 'x86_64' ]; then
 	PKG_TYPE='x86_64.pkg.tar.zst'
 else
@@ -15,35 +13,38 @@ QT6_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/c
 LIBXML_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/libxml2-iculess-$PKG_TYPE"
 OPUS_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/opus-nano-$PKG_TYPE"
 MESA_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/mesa-mini-$PKG_TYPE"
+INTEL_MEDIA_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/intel-media-mini-$PKG_TYPE" 
 
 echo "Installing build dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	base-devel \
-	curl \
+	base-devel         \
+	curl               \
 	desktop-file-utils \
-	git \
+	git                \
 	intel-media-driver \
-	libfdk-aac \
-	libxtst \
-	obs-studio \
-	pipewire-audio \
-	pulseaudio \
-	pulseaudio-alsa \
+	libfdk-aac         \
+	libva-intel-driver \
+	libxtst            \
+	obs-studio         \
+	pipewire-audio     \
+	pulseaudio         \
+	pulseaudio-alsa    \
 	qrcodegencpp-cmake \
-	qt6ct \
-	qt6-wayland \
-	wget \
-	xorg-server-xvfb \
+	qt6ct              \
+	qt6-wayland        \
+	wget               \
+	xorg-server-xvfb   \
 	zsync
 
 echo "Installing debloated pckages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$LLVM_URL"   -O  ./llvm-libs.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$QT6_URL"    -O  ./qt6-base.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$LIBXML_URL" -O  ./libxml2.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$OPUS_URL"   -O  ./opus.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$MESA_URL"   -O  ./mesa.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$LLVM_URL"         -O  ./llvm-libs.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$QT6_URL"          -O  ./qt6-base.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$LIBXML_URL"       -O  ./libxml2.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$OPUS_URL"         -O  ./opus.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$MESA_URL"         -O  ./mesa.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$INTEL_MEDIA_URL"  -O  ./intel-media.pkg.tar.zst
 
 pacman -U --noconfirm ./*.pkg.tar.zst
 rm -f ./*.pkg.tar.zst
