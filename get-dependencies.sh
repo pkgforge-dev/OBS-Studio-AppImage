cdd43833b789081d55d73b88f92ecc6df8201e24
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-25 19:51:49 -0400

    simplify deps install

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,30 +1,16 @@
 #!/bin/sh
 
 set -eux
-
-if [ "$(uname -m)" = 'x86_64' ]; then
-	PKG_TYPE='x86_64.pkg.tar.zst'
-else
-	PKG_TYPE='aarch64.pkg.tar.xz'
-fi
-
-LLVM_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/llvm-libs-nano-$PKG_TYPE"
-QT6_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/qt6-base-iculess-$PKG_TYPE"
-LIBXML_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/libxml2-iculess-$PKG_TYPE"
-OPUS_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/opus-nano-$PKG_TYPE"
-MESA_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/mesa-mini-$PKG_TYPE"
-INTEL_MEDIA_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/intel-media-mini-$PKG_TYPE" 
+ARCH="$(uname -m)"
+EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
 
 echo "Installing build dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
 	base-devel         \
 	curl               \
-	desktop-file-utils \
 	git                \
-	intel-media-driver \
 	libfdk-aac         \
-	libva-intel-driver \
 	libxtst            \
 	obs-studio         \
 	pipewire-audio     \
@@ -37,18 +23,12 @@ pacman -Syu --noconfirm \
 	xorg-server-xvfb   \
 	zsync
 
-echo "Installing debloated pckages..."
+if [ "$ARCH" = 'x86_64' ]; then
+		pacman -Syu --noconfirm libva-intel-driver
+fi
+
+echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$LLVM_URL"         -O  ./llvm-libs.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$QT6_URL"          -O  ./qt6-base.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$LIBXML_URL"       -O  ./libxml2.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$OPUS_URL"         -O  ./opus.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$MESA_URL"         -O  ./mesa.pkg.tar.zst
-wget --retry-connrefused --tries=30 "$INTEL_MEDIA_URL"  -O  ./intel-media.pkg.tar.zst
-
-pacman -U --noconfirm ./*.pkg.tar.zst
-rm -f ./*.pkg.tar.zst
-
-
-echo "All done!"
-echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
+chmod +x ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh --add-mesa llvm-libs-nano libxml2-mini qt6-base-mini gtk3-mini opus-mini
