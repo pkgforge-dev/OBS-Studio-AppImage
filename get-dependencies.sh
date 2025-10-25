8507cc48d3f277b697ecf54a9e9b5f311402915c
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-25 16:43:08 -0400

    use common packages

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -31,4 +31,4 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-mesa llvm-libs-nano libxml2-mini qt6-base-mini gtk3-mini opus-mini
+./get-debloated-pkgs.sh --add-common --prefer-nano intel-media-driver-mini
