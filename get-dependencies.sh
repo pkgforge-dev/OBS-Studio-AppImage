a7c7caa462739f73491a81a5d94e36b035179fad
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-15 02:37:19 -0400

    no longer needed

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -21,9 +21,7 @@ fi
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
-chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-common --prefer-nano intel-media-driver-mini
+get-debloated-pkgs --add-common --prefer-nano intel-media-driver-mini
 
 # Comment this out if you need an AUR package
 #get-aur-package PACKAGENAME
