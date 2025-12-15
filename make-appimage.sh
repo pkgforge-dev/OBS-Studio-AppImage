802fd9a4b27fb0de0b02229d836ce3492458fc58
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-15 02:28:26 -0400

    refactor

--- make-appimage.sh
+++ make-appimage.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
-ARCH="$(uname -m)"
-URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
-SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
-
-VERSION="$(pacman -Q obs-studio | awk 'NR==1 {print $2; exit}')"
-[ -n "$VERSION" ] && echo "$VERSION" > ~/version
-
+ARCH=$(uname -m)
+VERSION=$(pacman -Q obs-studio | awk '{print $2; exit}') # example command to get version of application here
+export ARCH VERSION
+export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
-export OUTNAME=OBS-Studio-"$VERSION"-anylinux-"$ARCH".AppImage
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export DESKTOP=/usr/share/applications/com.obsproject.Studio.desktop
 export ICON=/usr/share/icons/hicolor/256x256/apps/com.obsproject.Studio.png 
@@ -18,15 +14,14 @@ export DEPLOY_LOCALE=1
 export DEPLOY_OPENGL=1 
 export DEPLOY_PIPEWIRE=1
 
-# ADD LIBRARIES
-wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
-chmod +x ./quick-sharun
-./quick-sharun /usr/bin/obs* /usr/lib/libobs* /usr/lib/obs-*/*
+# Deploy dependencies
+./quick-sharun \
+	/usr/bin/obs*  \
+	/usr/lib/obs*  \
+	/usr/lib/libobs*
 
-# MAKE APPIMAGE WITH URUNTIME
-wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
-chmod +x ./uruntime2appimage
-./uruntime2appimage
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
 
 # make appbundle
 UPINFO="$(echo "$UPINFO" | sed 's#.AppImage.zsync#*.AppBundle.zsync#g')"
@@ -42,4 +37,5 @@ echo "Generating [dwfs]AppBundle..."
 	--compression "-C zstd:level=22 -S26 -B8" \
 	--output-to "OBS-Studio-$VERSION-anylinux-$ARCH.dwfs.AppBundle"
 zsyncmake ./*.AppBundle -u ./*.AppBundle
+mv -v ./*.AppBundle* ./dist
 echo "All Done!"
