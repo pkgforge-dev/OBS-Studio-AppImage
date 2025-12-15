98a153b838d5d6a8cabbc95acd1721e5300992bd
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-15 02:30:32 -0400

    refactor

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,26 +1,19 @@
 #!/bin/sh
 
-set -eux
-ARCH="$(uname -m)"
-EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+set -eu
 
-echo "Installing build dependencies..."
+ARCH=$(uname -m)
+
+echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	base-devel         \
-	curl               \
-	git                \
 	libfdk-aac         \
 	libxtst            \
 	obs-studio         \
 	pipewire-audio     \
-	pulseaudio         \
-	pulseaudio-alsa    \
+	pipewire-jack      \
 	qrcodegencpp-cmake \
-	qt6ct              \
-	wget               \
-	xorg-server-xvfb   \
-	zsync
+	qt6ct
 
 if [ "$ARCH" = 'x86_64' ]; then
 		pacman -Syu --noconfirm libva-intel-driver
@@ -31,3 +24,8 @@ echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
 ./get-debloated-pkgs.sh --add-common --prefer-nano intel-media-driver-mini
+
+# Comment this out if you need an AUR package
+#get-aur-package PACKAGENAME
+
+# If the application needs to be manually built that has to be done down here
