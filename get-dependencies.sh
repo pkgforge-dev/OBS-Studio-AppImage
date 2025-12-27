8a667c9420e950354ead9882e0a818b0e41da398
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-26 20:24:09 -0400

    oops

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -25,7 +25,7 @@ get-debloated-pkgs --add-common --prefer-nano intel-media-driver-mini
 
 # TODO do proper matrix to make separate build
 if true; then 
-	git clone --depth 1 https://aur.archlinux.org/obs-studio-browser.git ./obs
+	git clone --depth 1 https://gitlab.archlinux.org/archlinux/packaging/packages/obs-studio.git ./obs
 	cd ./obs
 	sed -i -e 's|-DENABLE_BROWSER=OFF|-DENABLE_BROWSER=ON -DCEF_ROOT_DIR="$srcdir"/../cef_binary_*|' ./PKGBUILD
 	wget "https://cdn-fastly.obsproject.com/downloads/cef_binary_6533_linux_${ARCH}_v6.tar.xz" -O /tmp/cef.tar.xz
