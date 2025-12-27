4ff592dd42bd12f3687841a2a01fc1a17d607e5b
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-26 20:09:08 -0400

    build browser thingy

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,7 +10,6 @@ pacman -Syu --noconfirm \
 	libfdk-aac         \
 	libxtst            \
 	luajit             \
-	obs-studio         \
 	pipewire-audio     \
 	pipewire-jack      \
 	qrcodegencpp-cmake \
@@ -24,7 +23,16 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano intel-media-driver-mini
 
-# Comment this out if you need an AUR package
-#get-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
+# TODO do proper matrix to make separate build
+if true; then 
+	git clone --depth 1 https://aur.archlinux.org/obs-studio-browser.git ./obs
+	cd ./obs
+	sed -i -e 's|-DENABLE_BROWSER=OFF|-DENABLE_BROWSER=ON -DCEF_ROOT_DIR="$srcdir"/../cef_binary_*|' ./PKGBUILD
+	wget "https://cdn-fastly.obsproject.com/downloads/cef_binary_6533_linux_${ARCH}_v6.tar.xz" -O /tmp/cef.tar.xz
+	tar xvf /tmp/cef.tar.xz
+	makepkg -fs --skippgpcheck
+	rm -f ./*debug*.pkg.tar*
+	pacman -U --noconfirm ./obs-studio*.pkg.tar.zst
+else
+	pacman -Syu --noconfirm obs-studio
+fi
