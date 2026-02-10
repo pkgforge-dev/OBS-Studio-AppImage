430723de94c566ae2691d7cf5f62c443a2db8b95
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-10 17:38:23 -0400

    update to template

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,11 +6,11 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q obs-studio | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
-export APPNAME=OBS_Studio
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export DESKTOP=/usr/share/applications/com.obsproject.Studio.desktop
-export ICON=/usr/share/icons/hicolor/256x256/apps/com.obsproject.Studio.png 
+export ICON=/usr/share/icons/hicolor/256x256/apps/com.obsproject.Studio.png
+export APPNAME=OBS_Studio
 export DEPLOY_LOCALE=1
 export DEPLOY_OPENGL=1 
 export DEPLOY_PIPEWIRE=1
@@ -24,22 +24,11 @@ quick-sharun \
 	/usr/lib/libobs* \
 	/usr/lib/libluajit*.so*
 
+# Additional changes can be done in between here
+
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
-# make appbundle
-UPINFO="$(echo "$UPINFO" | sed 's#.AppImage.zsync#*.AppBundle.zsync#g')"
-wget --retry-connrefused --tries=30 \
-	"https://github.com/xplshn/pelf/releases/latest/download/pelf_$ARCH" -O ./pelf
-chmod +x ./pelf
-echo "Generating [dwfs]AppBundle..."
-./pelf --add-appdir ./AppDir \
-	--appbundle-id="com.obsproject.Studio#github.com/$GITHUB_REPOSITORY:$VERSION@$(date +%d_%m_%Y)" \
-	--appimage-compat \
-	--disable-use-random-workdir \
-	--add-updinfo "$UPINFO" \
-	--compression "-C zstd:level=22 -S26 -B8" \
-	--output-to "$APPNAME-$VERSION-anylinux-$ARCH.dwfs.AppBundle"
-zsyncmake ./*.AppBundle -u ./*.AppBundle
-mv -v ./*.AppBundle* ./dist
-echo "All Done!"
+# Test the app for 12 seconds, if the app normally quits before that time
+# then skip this or check if some flag can be passed that makes it stay open
+quick-sharun --test ./dist/*.AppImage
